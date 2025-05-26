53c60602c311dacc6e657d968d73e4b65bcfba7a
erikamilenazipa <mile.zipa96@gmail.com>
2025-05-26 11:51:30 -0500

    Create dim_meses_create.sql

---
+++ DataWarehouse/Scripts/Scripts/dim_meses_create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE DataWarehouse.dbo.dim_Meses (
+	Mes tinyint IDENTITY(1,1) NOT NULL,
+	NomMes nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	NomCortoMes char(3) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	CONSTRAINT DIM_Meses_PK PRIMARY KEY (Mes)
+);
