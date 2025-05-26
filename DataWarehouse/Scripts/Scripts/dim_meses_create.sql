1ee6dffae1ee9eb2bd6425592070d8538dbf7767
erikamilenazipa <mile.zipa96@gmail.com>
2025-05-26 13:08:52 -0500

    Update dim_meses_create.sql

--- DataWarehouse/Scripts/Scripts/dim_meses_create.sql
+++ DataWarehouse/Scripts/Scripts/dim_meses_create.sql
@@ -1,3 +1,6 @@
+-- Crear tabla de productos con campos Mes, NomMes, NomCortoMes.
+-- Es importante definir claves primarias y restricciones de integridad para garantizar datos consistentes y facilitar las consultas.
+
 CREATE TABLE DataWarehouse.dbo.dim_Meses (
 	Mes tinyint IDENTITY(1,1) NOT NULL,
 	NomMes nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
