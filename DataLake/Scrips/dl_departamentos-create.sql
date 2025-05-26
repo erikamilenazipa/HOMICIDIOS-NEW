3a60cbd94b233da99d5f5f4ba1dee7216d34ac08
erikamilenazipa <mile.zipa96@gmail.com>
2025-05-26 14:00:18 -0500

    Update dl_departamentos-create.sql

--- DataLake/Scrips/dl_departamentos-create.sql
+++ DataLake/Scrips/dl_departamentos-create.sql
@@ -1,3 +1,6 @@
+-- Crear tabla de productos con campos COD_DPTO, NOM_DPTO, LATITUD, LONGITUD, GeoDepartamento.
+-- Es importante definir claves primarias y restricciones de integridad para garantizar datos consistentes y facilitar las consultas.
+
 IF OBJECT_ID('dbo.DL_Departamentos', 'U') IS NULL
 BEGIN
 	CREATE TABLE dbo.DL_Departamentos (
