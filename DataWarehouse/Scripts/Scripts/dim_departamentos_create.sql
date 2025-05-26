ba5e76b23c65dfc91fb8064df5acaa49f96a1809
erikamilenazipa <mile.zipa96@gmail.com>
2025-05-26 13:06:13 -0500

    Update dim_departamentos_create.sql

--- DataWarehouse/Scripts/Scripts/dim_departamentos_create.sql
+++ DataWarehouse/Scripts/Scripts/dim_departamentos_create.sql
@@ -1,3 +1,6 @@
+-- Crear tabla de productos con campos CodDpto, NomDpto, Latitud , Longitud. 
+-- Es importante definir claves primarias y restricciones de integridad para garantizar datos consistentes y facilitar las consultas.
+
 CREATE TABLE DataWarehouse.dbo.dim_Departamentos (
 	CodDpto tinyint NOT NULL,
 	NomDpto nvarchar(60) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
