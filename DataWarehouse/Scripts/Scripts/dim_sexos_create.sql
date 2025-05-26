7fb6d34b343be97338ec267a7018a6d0b9154f12
erikamilenazipa <mile.zipa96@gmail.com>
2025-05-26 13:10:15 -0500

    Update dim_sexos_create.sql

--- DataWarehouse/Scripts/Scripts/dim_sexos_create.sql
+++ DataWarehouse/Scripts/Scripts/dim_sexos_create.sql
@@ -1,3 +1,7 @@
+-- Crear tabla de productos con campos id_sexo, NomSexo.
+-- Es importante definir claves primarias y restricciones de integridad para garantizar datos consistentes y facilitar las consultas.
+
+
 CREATE TABLE DataWarehouse.dbo.dim_sexo (
 	id_sexo tinyint IDENTITY(1,1) NOT NULL,
 	NomSexo nvarchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
