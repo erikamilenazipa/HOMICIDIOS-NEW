2a5391160e21145eaad76260c162cbe218537704
erikamilenazipa <mile.zipa96@gmail.com>
2025-05-26 13:06:52 -0500

    Update dim_dias_semana_create.sql

--- DataWarehouse/Scripts/Scripts/dim_dias_semana_create.sql
+++ DataWarehouse/Scripts/Scripts/dim_dias_semana_create.sql
@@ -1,3 +1,6 @@
+-- Crear tabla de productos con campos DiaSemana, NomDiaSemana, NomCortoDiaSemana.
+-- Es importante definir claves primarias y restricciones de integridad para garantizar datos consistentes y facilitar las consultas.
+
 CREATE TABLE DataWarehouse.dbo.dim_Dias_Semana (
 	DiaSemana tinyint IDENTITY(1,1) NOT NULL,
 	NomDiaSemana nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
