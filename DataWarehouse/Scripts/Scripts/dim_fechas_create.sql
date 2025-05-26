7ed45d31b1f9010cc3932d1972840173890caf43
erikamilenazipa <mile.zipa96@gmail.com>
2025-05-26 11:49:59 -0500

    Create dim_fechas_create.sql

---
+++ DataWarehouse/Scripts/Scripts/dim_fechas_create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE DataWarehouse.dbo.dim_fechas (
+	Annio int NOT NULL,
+	Mes tinyint NOT NULL,
+	Dia tinyint NOT NULL,
+	Semana tinyint NOT NULL,
+	Trimestre tinyint NOT NULL,
+	DiaSeamama tinyint NOT NULL,
+	fechaDeHecho date NOT NULL,
+	CONSTRAINT dim_fechas_pk PRIMARY KEY (fechaDeHecho),
+	CONSTRAINT dim_fechas_dim_Dias_Semana_FK FOREIGN KEY (DiaSeamama) REFERENCES DataWarehouse.dbo.dim_Dias_Semana(DiaSemana),
+	CONSTRAINT dim_fechas_dim_Meses_FK FOREIGN KEY (Mes) REFERENCES DataWarehouse.dbo.dim_Meses(Mes),
+	CONSTRAINT dim_fechas_dim_Trimestres_FK FOREIGN KEY (Trimestre) REFERENCES DataWarehouse.dbo.dim_Trimestres(Trimestre)
+);
