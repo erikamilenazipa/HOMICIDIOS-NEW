24781d48d0a909bb6935215e8ee8a8c1b8042cc0
erikamilenazipa <mile.zipa96@gmail.com>
2025-05-26 13:10:42 -0500

    Update dim_trimestres_create.sql

--- DataWarehouse/Scripts/Scripts/dim_trimestres_create.sql
+++ DataWarehouse/Scripts/Scripts/dim_trimestres_create.sql
@@ -1,3 +1,7 @@
+-- Crear tabla de productos con campos Trimestre, NomTrimestre, NomCortoTrimestre.
+-- Es importante definir claves primarias y restricciones de integridad para garantizar datos consistentes y facilitar las consultas.
+
+
 CREATE TABLE DataWarehouse.dbo.dim_Trimestres (
 	Trimestre tinyint IDENTITY(1,1) NOT NULL,
 	NomTrimestre nvarchar(15) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
