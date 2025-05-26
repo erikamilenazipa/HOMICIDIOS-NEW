a1bee0fe786ae4d1f0d3bbfcf30c88cdbe4da7f5
erikamilenazipa <mile.zipa96@gmail.com>
2025-05-26 13:11:10 -0500

    Update dim_zonas_create.sql

--- DataWarehouse/Scripts/Scripts/dim_zonas_create.sql
+++ DataWarehouse/Scripts/Scripts/dim_zonas_create.sql
@@ -1,3 +1,7 @@
+-- Crear tabla de productos con campos id_zona, nom_zona.
+-- Es importante definir claves primarias y restricciones de integridad para garantizar datos consistentes y facilitar las consultas.
+
+
 CREATE TABLE DataWarehouse.dbo.dm_zonas (
 	id_zona tinyint IDENTITY(1,1) NOT NULL,
 	nom_zona nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
